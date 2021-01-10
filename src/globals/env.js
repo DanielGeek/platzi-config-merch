0ec4dc8d49ff3cc224b5bc157f80de14de5f871c
DanielGeek <daniel30081990@gmail.com>
2021-01-10 19:55:28 -0300

    update env.js

--- src/globals/env.js
+++ src/globals/env.js
@@ -1,4 +1,4 @@
-// const functions = require('firebase-functions');
+const functions = require('firebase-functions');
 // const admin = require('firebase-admin');
 
 let REACT_APP_PAYPAL_CLIENT_ID = '';
@@ -11,8 +11,11 @@ switch (process.env.NODE_ENV) {
     break;
   case 'production':
     // get variables de entorno firebase:
-    // REACT_APP_PAYPAL_CLIENT_ID = functions.config().google.react_app_paypal_client_id;
-    // REACT_APP_GOOGLE_MAPS_API_KEY = functions.config().google.react_app_google_maps_api_key;
+    REACT_APP_PAYPAL_CLIENT_ID = functions.config().google
+      .react_app_paypal_client_id;
+    REACT_APP_GOOGLE_MAPS_API_KEY = functions.config().google
+      .react_app_google_maps_api_key;
+
     break;
 }
 
