f5f2fe2bec4f67a9f8842814275f7dcb8b244aea
DanielGeek <daniel30081990@gmail.com>
2021-01-10 04:23:57 -0300

    asasasas

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,11 +1,11 @@
-const functions = require('firebase-functions');
+import functions from 'firebase-functions';
 
 functions.config().google.react_app_paypal_client_id;
 functions.config().google.react_app_google_maps_api_key;
 
 const REACT_APP_PAYPAL_CLIENT_ID = functions.config().google
-  .react_app_paypal_client_id;
+    .react_app_paypal_client_id;
 const REACT_APP_GOOGLE_MAPS_API_KEY = functions.config().google
-  .react_app_google_maps_api_key;
+    .react_app_google_maps_api_key;
 
 export { REACT_APP_PAYPAL_CLIENT_ID, REACT_APP_GOOGLE_MAPS_API_KEY };
